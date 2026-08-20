9d119d2c0a6fd8f20ca980c1eecfdb362c8b1b06
butterflycxmillaa <camilla.butterflydev@gmail.com>
2026-08-20 15:42:53 +0200

    state management for known primes array

--- _primefact.py
+++ _primefact.py
@@ -2,21 +2,22 @@ import math
 from sortedcontainers import SortedDict
 from _bigint import big_exp_mod_N
 
-class NumberGraphNode:
-    def __init__(self, value: int):
-        self.value = value
+known_primes = []
+upper_lim = 0
 
-def sieve_of_eratosthenes(n: int) -> list[bool]:
-    # Returns a list is_prime where is_prime[i] shows whether (i + 1) <is prime
+def sieve_of_eratosthenes(n: int) -> list[int]:
+    # returns the list of primes p such that p <= n
     is_prime = [True] * n
     for num in range(2, int(n ** 0.5) + 1):
         if is_prime[num - 1]:
             # iterate over all the multiples of num
             for mul in range(num * 2, n + 1, num):
                 is_prime[mul - 1] = False
-    return is_prime
+    return [i + 1 for i in range(len(is_prime)) if is_prime[i] and i != 0]
 
 def segmented_soe(kp: list[int], L: int) -> list[int]:
+    if not kp:
+        return sieve_of_eratosthenes(L)
     M = kp[-1] + 1
     if L < M: return []
     # candidates[0] points to M
@@ -31,40 +32,20 @@ def segmented_soe(kp: list[int], L: int) -> list[int]:
             candidates[i] = False
     return [M + i for i in range(dim) if candidates[i]]
 
-prime_map = sieve_of_eratosthenes(1_000_000)
-known_primes = list[int]()
-number_dict = SortedDict[int, int]()
-node_arr = list[NumberGraphNode]()
-n_nodes = 0
-for i in range(1, len(prime_map)):
-    if prime_map[i]:
-        known_primes.append(i + 1)
-        print(i + 1, end = " ")
-more_primes = segmented_soe(known_primes, 11_000_000)
-for p in more_primes:
+def set_upper_lim_kp(lim: int) -> list[int]:
+    global known_primes, upper_lim
+    if upper_lim == 0:
+        sqrt_lim = int(math.isqrt(lim) + 1)
+        known_primes = sieve_of_eratosthenes(sqrt_lim)
+    expand_primes = segmented_soe(known_primes, lim)
+    known_primes += expand_primes
+    upper_lim = lim
+    return known_primes
+
+kp = set_upper_lim_kp(10_000_000)
+for p in kp:
     print(p, end = " ")
 print()
-
-def is_num_present(num: int) -> bool:
-    return num in number_dict
-
-def return_num_index(num: int) -> int:
-    return number_dict[num] if is_num_present(num) else -1
-
-# returns the index of the node representing the number
-# if it doesn't exist, it creates a new node and returns its index
-def insert_num(num: int) -> int:
-    if is_num_present(num):
-        return number_dict[num]
-    else:
-        try:
-            node_arr.append(NumberGraphNode(num))
-            number_dict[num] = n_nodes
-            n_nodes += 1
-            return n_nodes - 1
-        except ValueError as e:
-            print(f"Invalid number: {num}. {e.args[0]}")
-            return -1
   
 def find_known_factors(num: int) -> list[int]:
     # factorizes using the known primes and returns a list of the factors found
